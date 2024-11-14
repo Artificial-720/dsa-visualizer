ad3a0c826f489e9d140051d764a3ef70c801bffc
Artificial-720 <76878013+Artificial-720@users.noreply.github.com>
2024-11-14 10:44:29 -0600

    updated tree traversal to use base tree view

--- views/tree_traversal_view.py
+++ views/tree_traversal_view.py
@@ -1,38 +1,15 @@
 import tkinter as tk
-from views.base_tree_view import BaseTreeView
-
-
-class Node():
-    def __init__(self, data):
-        self.data = data
-        self.left = None
-        self.right = None
-
-
-class QueueObject():
-    """ Object used for holding data in queue during draw loop """
-
-    def __init__(self, node, x, y, offset_x):
-        self.node = node
-        self.x = x
-        self.y = y
-        self.offset_x = offset_x
+from views.base_tree_view import BaseTreeView, Node
 
 
 class TreeTraversalView(BaseTreeView):
 
-    CANVAS_WIDTH = 400
-    CANVAS_HEIGHT = 400
-
     def __init__(self, parent, controller):
+        self.title = "Tree Traversal"
+        self.description = "Tree Traversal defines a way to visit each node in a tree. In-order (left, root, right), Pre-order (root, left, right), Post-order (left, right, root)"
+
         self.root = None
 
-        self.animation_delay = 500
-        self.node_diameter = 30
-        self.offset_x = 60
-        self.offset_y = 60
-
-        self.node_radius = self.node_diameter / 2
         self.setup_test_tree()
 
         super().__init__(parent, controller)
@@ -58,107 +35,25 @@ class TreeTraversalView(BaseTreeView):
 
         nodeF.left = nodeG
 
-    def tkraise(self):
-        """Override tkraise to draw frame when it loads"""
-        super().tkraise()
-        self.draw()
-
-    def create_info_frame(self):
-        """Implement create_info_frame. Creates and configures the information frame."""
-        frame = tk.Frame(self, bg=self.COLOR_BG, pady=10, padx=10)
-        self.canvas = tk.Canvas(frame, width=self.CANVAS_WIDTH, height=self.CANVAS_HEIGHT, bg="white")
-        self.canvas.pack(pady=20)
+    def create_controls_frame(self, parent):
+        control_frame = tk.Frame(parent)
 
         # Frame to results label
-        result_frame = tk.Frame(frame)
+        result_frame = tk.Frame(control_frame)
         result_frame.pack()
-
         # label for output
         tk.Label(result_frame, text="Results: ").pack(side=tk.LEFT)
         self.label = tk.Label(result_frame, text="")
         self.label.pack(pady=10, anchor=tk.W)
 
-        # Frame to hold buttons
-        button_frame = tk.Frame(frame)
+        button_frame = tk.Frame(control_frame)
         button_frame.pack()
-
         # Buttons
         tk.Button(button_frame, text="Pre-order Traversal", command=self.pre_order_traversal_button).grid(row=0, column=0, padx=10)
         tk.Button(button_frame, text="In-order Traversal", command=self.in_order_traversal_button).grid(row=0, column=1, padx=10)
         tk.Button(button_frame, text="Post-order Traversal", command=self.post_order_traversal_button).grid(row=0, column=2, padx=10)
 
-        return frame
-
-    def draw(self, checking=[], checked=[]):
-        # Clear canvas
-        self.canvas.delete("all")
-
-        # Tree is empty
-        if not self.root:
-            return
-
-        start_x = (self.canvas.winfo_width() // 2) - self.node_radius
-        x, y = start_x, 10
-
-        q = []
-        q.append(QueueObject(self.root, x, y, self.offset_x))
-        while len(q) > 0:
-            queue_object = q.pop(0)
-            x, y = queue_object.x, queue_object.y
-            """
-            checking: solid green
-            checked: faded green
-            not checked: gray
-            """
-            color = "LightBlue"
-            if queue_object.node in checking:
-                color = "LimeGreen"
-            if queue_object.node in checked:
-                color = "SpringGreen"
-            # Draw circle
-            self.canvas.create_oval(x, y, x + self.node_diameter, y + self.node_diameter, fill=color)
-            # Draw text
-            self.canvas.create_text(x + self.node_radius, y + self.node_radius, text=queue_object.node.data)
-
-            y += self.offset_y
-
-            def calc_edge(point, multiple=1):
-                return (point + self.node_radius) + self.node_radius * 0.707106781 * multiple
-            # Draw left node
-            if queue_object.node.left:
-                left_object = QueueObject(queue_object.node.left, x - queue_object.offset_x, y, queue_object.offset_x // 2)
-                q.append(left_object)
-                # Draw line
-                self.canvas.create_line(calc_edge(queue_object.x, -1),
-                                        calc_edge(queue_object.y),
-                                        calc_edge(left_object.x),
-                                        calc_edge(left_object.y, -1),
-                                        arrow="last")
-            # Draw right node
-            if queue_object.node.right:
-                right_object = QueueObject(queue_object.node.right, x + queue_object.offset_x, y, queue_object.offset_x // 2)
-                q.append(right_object)
-                # Draw line
-                self.canvas.create_line(calc_edge(queue_object.x),
-                                        calc_edge(queue_object.y),
-                                        calc_edge(right_object.x, -1),
-                                        calc_edge(right_object.y, -1),
-                                        arrow="last")
-
-    def animate(self, gen):
-        """Animate algorithm by updating the canvas."""
-        self.label["text"] = ""
-
-        def step():
-            try:
-                checking, checked = next(gen)
-                self.draw(checking, checked)
-                self.after(self.animation_delay, step)
-            except StopIteration:
-                self.animation_finished = True
-
-        # Inital start of animation
-        step()
+        return control_frame
 
     def pre_order_traversal_button(self):
         self.animate(self.pre_order_traversal_generator(self.root))
