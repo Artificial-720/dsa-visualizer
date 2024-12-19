628aea2dc57591b51d938e424cea74e767ac781f
Artificial-720 <76878013+Artificial-720@users.noreply.github.com>
2024-12-19 17:50:20 -0600

    fixed button label in kruskal view

--- views/graph/graph_minimum_spanning_tree_kruskals_view.py
+++ views/graph/graph_minimum_spanning_tree_kruskals_view.py
@@ -46,13 +46,13 @@ class GraphMinimumSpanningTreeKruskalView(BaseGraphView):
         # Buttons
         button_frame = tk.Frame(controls_frame)
         button_frame.pack()
-        tk.Button(button_frame, text="Run Prim's", command=self.prim_button).grid(row=0, column=0, padx=10)
+        tk.Button(button_frame, text="Run Kruskal's", command=self.kruskal_button).grid(row=0, column=0, padx=10)
         return controls_frame
 
-    def prim_button(self):
-        self.animate(self.prim_generator())
+    def kruskal_button(self):
+        self.animate(self.kruskal_generator())
 
-    def prim_generator(self):
+    def kruskal_generator(self):
         n = len(self.vertex_data)
         result = []
         i = 0
